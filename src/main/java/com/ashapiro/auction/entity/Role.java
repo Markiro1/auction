ab8d7e079938ea947a4ff56e895621194c9b5292
Markiro1 <65sumo@gmail.com>
2024-03-01 13:13:01 +0200

    - added field user to Role.class and mapping on the child side

--- src/main/java/com/ashapiro/auction/entity/Role.java
+++ src/main/java/com/ashapiro/auction/entity/Role.java
@@ -1,8 +1,12 @@
 package com.ashapiro.auction.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "roles")
@@ -14,4 +18,13 @@ public class Role {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @Setter(AccessLevel.PRIVATE)
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> users = new ArrayList<>();
 }
