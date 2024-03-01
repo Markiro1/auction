bdc44817e9d1ff8c224036aa44a800ac85470918
Markiro1 <65sumo@gmail.com>
2024-03-01 13:14:33 +0200

    - added method findUserByEmail() with fetch roles - added method existsByEmail()

--- src/main/java/com/ashapiro/auction/repository/UserRepository.java
+++ src/main/java/com/ashapiro/auction/repository/UserRepository.java
@@ -2,7 +2,14 @@ package com.ashapiro.auction.repository;
 
 import com.ashapiro.auction.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("select u from User u left join fetch u.roles")
+    Optional<User> findUserByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
