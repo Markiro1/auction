163b077b80dec404c698d9c7c66f603ca4ac9dc1
Markiro1 <65sumo@gmail.com>
2024-03-11 12:48:21 +0200

    - added new method in auction repository

--- src/main/java/com/ashapiro/auction/repository/AuctionRepository.java
+++ src/main/java/com/ashapiro/auction/repository/AuctionRepository.java
@@ -1,7 +1,25 @@
 package com.ashapiro.auction.repository;
 
+import com.ashapiro.auction.dto.auction.AuctionDto;
+import com.ashapiro.auction.dto.product.ProductDto;
 import com.ashapiro.auction.entity.Auction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AuctionRepository  extends JpaRepository<Auction, Long> {
+
+    @Query("select new com.ashapiro.auction.dto.auction.AuctionDto(" +
+            "a.id, " +
+            "p.name, " +
+            "p.id, " +
+            "u.email, " +
+            "u.id, " +
+            "a.status," +
+            "a.localDateTime as createdAt"+
+            ") from Auction a " +
+            "left join a.product p " +
+            "left join p.owner u")
+    List<AuctionDto> findAllAuctions();
 }
