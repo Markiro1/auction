3c677220d864f9a5652bdb914a866297121a2fd1
Markiro1 <65sumo@gmail.com>
2024-03-11 12:50:00 +0200

    - added new method findByEmail()

--- src/main/java/com/ashapiro/auction/service/UserService.java
+++ src/main/java/com/ashapiro/auction/service/UserService.java
@@ -2,8 +2,13 @@ package com.ashapiro.auction.service;
 
 import com.ashapiro.auction.dto.registration.RegisterUserDto;
 import com.ashapiro.auction.dto.registration.UserResponseDto;
+import com.ashapiro.auction.entity.User;
+
+import java.util.Optional;
 
 public interface UserService {
 
     UserResponseDto save(RegisterUserDto userDto);
+
+    Optional<User> findByEmail(String email);
 }
