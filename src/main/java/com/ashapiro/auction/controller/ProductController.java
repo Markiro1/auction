88006fe607db3e807df8fdc9dd6c2e68486d4832
Markiro1 <65sumo@gmail.com>
2024-03-13 14:48:19 +0200

    - added new methods and replace ProductDto to ProductWithEmailDto

--- src/main/java/com/ashapiro/auction/controller/ProductController.java
+++ src/main/java/com/ashapiro/auction/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.ashapiro.auction.controller;
 
 import com.ashapiro.auction.dto.product.ProductDto;
+import com.ashapiro.auction.dto.product.ProductWithEmailDto;
+import com.ashapiro.auction.dto.product.SimpleProductDto;
 import com.ashapiro.auction.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/products")
@@ -15,12 +18,23 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/add")
-    public void add(@RequestBody ProductDto productDto) {
-        productService.add(productDto);
+    public void add(@RequestBody ProductWithEmailDto productWithEmailDto) {
+        productService.add(productWithEmailDto);
     }
 
     @GetMapping("/all")
-    public List<ProductDto> getAll() {
+    public List<ProductWithEmailDto> getAll() {
         return productService.getAllProductsWithUserEmail();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDto getProduct(@PathVariable Long id) {
+        return productService.getProductById(id)
+                .orElseThrow(() -> new NoSuchElementException(String.format("Product with id: %d does not exists", id)));
+    }
+
+    @GetMapping("/user/{id}")
+    public List<SimpleProductDto> getProductsByUserId(@PathVariable Long id) {
+        return productService.getProductsByUserId(id);
     }
 }
