962e6faf5d5104259ed33ae7b3ffc2452cea73b2
Markiro1 <65sumo@gmail.com>
2024-03-13 14:49:40 +0200

    - added new field "id"

--- src/main/java/com/ashapiro/auction/userDetails/UserDetailsImpl.java
+++ src/main/java/com/ashapiro/auction/userDetails/UserDetailsImpl.java
@@ -15,6 +15,7 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetails {
 
+    private final Long id;
     private final String email;
     private final String password;
     private final Set<SimpleGrantedAuthority> roles;
